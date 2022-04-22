88223c9deb99aa642170b144790690c2fa79871e
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2022-04-22 15:17:03 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,19 +11,10 @@
 
 int main(void)
 {
-	int d;
-
-	for (d = 0; d < 100; d++)
-	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-
-	return (0);
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	
+	putchar(*\n*);
+	return(0);
 }
